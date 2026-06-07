026dd529d76f85b8222e1bf384d0dd416ba7b8fd
vyaskrisha1-del <vyaskrisha1@gmail.com>
2026-06-07 13:58:07 +0530

    update project

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -119,14 +120,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = 'bbcare1402@gmail.com'
-EMAIL_HOST_PASSWORD = 'lmmiwxqsksnclgvs'
+EMAIL_HOST_USER = os.environ.get('bbcare1402@gmail.com')
+EMAIL_HOST_PASSWORD = os.environ.get('lmmiwxqsksnclgvs')
+EMAIL_TIMEOUT = 10
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
