f72d8a7679fbf60a187513873cfb8ae6ca040134
vyaskrisha1-del <vyaskrisha1@gmail.com>
2026-06-07 14:14:49 +0530

    update project

--- config/settings.py
+++ config/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-    
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 465
@@ -128,7 +128,7 @@ EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
 EMAIL_HOST_USER = os.environ.get('bbcare1402@gmail.com')
 EMAIL_HOST_PASSWORD = os.environ.get('lmmiwxqsksnclgvs')
-EMAIL_TIMEOUT = 10
+EMAIL_TIMEOUT = 5
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
