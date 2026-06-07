46a7f0e0e33da003094f9d358dbc9ba181e93d1b
vyaskrisha1-del <vyaskrisha1@gmail.com>
2026-06-07 14:29:02 +0530

    update project

--- config/settings.py
+++ config/settings.py
@@ -125,7 +125,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT =  587
 EMAIL_USE_TLS = True
-EMAIL_USE_SSL = True
+
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'bbcare1402@gmail.com')  # ✅ Correct
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'lmmiwxqsksnclgvs')  # ✅ Correct
 EMAIL_TIMEOUT = 5
