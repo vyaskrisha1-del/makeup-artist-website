d0a66ba19c689e86d0b91211655e336e1531a025
vyaskrisha1-del <vyaskrisha1@gmail.com>
2026-06-07 15:45:32 +0530

    update project

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -125,10 +125,11 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT =  587
 EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 
 EMAIL_HOST_USER = 'bbcare1402@gmail.com'
 EMAIL_HOST_PASSWORD =  'lmmiwxqsksnclgvs'
-EMAIL_TIMEOUT = 5
+
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
